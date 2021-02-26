3a57f0f840d854d31d301946986b4c12fb2751bc
Annelies <0962949@hr.nl>
2021-02-26 13:23:24 +0100

    Fixed bug flashing points

--- PinkPoint.cpp
+++ PinkPoint.cpp
@@ -2,10 +2,10 @@
 #include <Arduino.h>
 #include <Adafruit_CircuitPlayground.h>
 
-#define LED
-
 void PinkPoint::update() {
-    CircuitPlayground.setPixelColor(x,100,0,100);
+    if(active) {
+      CircuitPlayground.setPixelColor(x,100,0,100);
+    }
 }
 
 PinkPoint::PinkPoint() {
--- YellowPoint.cpp
+++ YellowPoint.cpp
@@ -2,11 +2,10 @@
 #include <Arduino.h>
 #include <Adafruit_CircuitPlayground.h>
 
-#define LED
-//int r = rand() %0 + 9;
 void YellowPoint::update() {
-//  playSong();
-  CircuitPlayground.setPixelColor(x,100,100,0);
+  if(active) {
+    CircuitPlayground.setPixelColor(x,100,100,0);
+  }
 }
 
 YellowPoint::YellowPoint() {
